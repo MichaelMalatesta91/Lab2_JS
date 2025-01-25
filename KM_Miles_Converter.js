42aaa9065006b9c02a1523cea9371ede99efe7ae
MichaelMalatesta91 <159482698+MichaelMalatesta91@users.noreply.github.com>
2025-01-25 11:17:42 -0500

    Update KM_Miles_Converter.js
    Added comments for my own memory/understanding

--- KM_Miles_Converter.js
+++ KM_Miles_Converter.js
@@ -1,10 +1,13 @@
-const readline = require('readline');
+const readline = require('readline'); 
+// starts readline which is a node.js tool for input/output
 
+//creates ability to input/output with readline
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+//Used to ask the question(s) & Calculate result, print to log
 rl.question("Enter distance (kilometers): ", (kilometers) => {
   const miles = parseFloat(kilometers) * 0.621371;
   console.log(`${kilometers} kilometers is equal to ${miles.toFixed(2)} miles.`);
